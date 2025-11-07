d68bf08e7e1112c5b7615a6d61d970eb3ab528aa
Joseph <10326622@upsamail.edu.gh>
2025-11-07 02:41:18 +0200

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,1 +1,25 @@
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
 
+// Create an object called row with the type RowElement
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+
+// Create a const variable named newRowID with the type RowID
+const newRowID: RowID = CRUD.insertRow(row);
+console.log(`New row ID: ${newRowID}`);
+
+// Create a const variable named updatedRow with the type RowElement
+const updatedRow: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+  age: 23,
+};
+
+// Call updateRow
+CRUD.updateRow(newRowID, updatedRow);
+
+// Call deleteRow
+CRUD.deleteRow(newRowID);
